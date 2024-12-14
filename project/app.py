eacb4be84fd175c50a483a394eb798644ca70b92
Bimani Sakti <bimanisakti123@gmail.com>
2024-12-14 20:47:50 +0700

    chore: Memperbarui template projek

--- project/app.py
+++ project/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config["MYSQL_HOST"] = "localhost"
-app.config["MYSQL_PORT"] = 3306
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = ""
+app.secret_key = os.environ.get('SECRET_KEY')
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT'))
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
 
 mysql = MySQL(app)
